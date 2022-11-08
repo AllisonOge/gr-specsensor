7d5a1e0ea4231d02cccbd62a76e8140dfa00db2e
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-11-08 14:41:44 +0000

    Added new proactive channel selection methods
    The following methods were added:
    - Hoyhtya
    - Renewal theory

--- python/specsensor/cs_methods.py
+++ python/specsensor/cs_methods.py
@@ -2,10 +2,88 @@ import random
 import tensorflow.lite as tflite
 import os
 import numpy as np
-from .utils import start_and_idle_time
+from .utils import start_and_idle_time, on_and_off_time
 
 
-class RandomChannelSelection:
+class CSMethods:
+    def __init__(self, name):
+        self.name = name
+        self.selected_channel = None
+
+    def select_channel(self, channel_state):
+        pass
+
+
+class ML(CSMethods):
+    """ML Parent Class to prepare the data and load model
+       Attributes
+       ----------
+            dataset: (Array-like) saves the latest channel state within window size
+            name: (string) name of the model
+            window_size:(int) length of channel state to store in memory (default=10)
+            model_interpreter: tflite interpreter to load model
+
+        Methods
+        -------
+            update_dataset(channel_state)
+
+    """
+
+    def __init__(self, name: str, window_size: int = 10, model_path: str = os.path.abspath("./models/model.tflite")):
+        self.dataset: list = []
+        self.window_size: int = window_size
+        CSMethods.__init__(self, name)
+        self.model_interpreter = tflite.Interpreter(model_path=model_path)
+        # allocate memory
+        self.model_interpreter.allocate_tensors()
+
+    def update_dataset(self, channel_state: list) -> None:
+        """Update the dataset buffer
+
+           Parameters
+           ----------
+                channel_state: (1-D Array-like) state of the channel
+
+            Return 
+            ------
+                None
+        """
+        self.dataset.append(channel_state)
+
+    def get_prediction(self, channel_state) -> None or dict:
+        """Prepare data and invoke model inference
+
+           Parameters
+           ----------
+                channel_state: (1-D Array-like) state of the channel
+
+            Return
+            ------
+                None or predictions
+        """
+        # update dataset and reshape to window size
+        self.update_dataset(channel_state)
+
+        if not len(self.dataset) >= self.window_size:
+            return
+
+        self.dataset = self.dataset[:self.window_size]
+
+        # set input tensor for the model
+        X = np.array([r for r in self.dataset], dtype=np.float32).reshape(
+            (-1, self.window_size, len(channel_state)))
+
+        idx = self.model_interpreter.get_input_details()[0]["index"]
+        # set input tensor
+        self.model_interpreter.set_tensor(idx, X)
+        # invoke predictions
+        self.model_interpreter.invoke()
+
+        idx = self.model_interpreter.get_output_details()[0]["index"]
+        return self.model_interpreter.get_tensor(idx)
+
+
+class RandomChannelSelection(CSMethods):
     """Random Channel Selection Method
        A channel selection method that chooses a channel from the 
        list of free channels randomly
@@ -22,8 +100,7 @@ class RandomChannelSelection:
     """
 
     def __init__(self):
-        self.selected_channel = None
-        self.name = "random"
+        CSMethods.__init__(self, "random")
 
     def select_channel(self, channel_state: list):
         """Randomly select a channel from the list of free channels
@@ -60,7 +137,7 @@ class RandomChannelSelection:
         self.selected_channel = selected_channel
 
 
-class NextOrPreviousChannelSelection:
+class NextOrPreviousChannelSelection(CSMethods):
     """Next or Previous Channel Selection
        A channel selection algorithm that selects the next/previous free channel
        from the list of free channels
@@ -76,9 +153,9 @@ class NextOrPreviousChannelSelection:
     """
 
     def __init__(self, state="next"):
-        self.selected_channel = None
         if state not in ["next", "prev"]:
             raise Exception("State can only be next or previous")
+        CSMethods.__init__(self, state)
         self.state = state
 
     def select_channel(self, channel_state):
@@ -134,79 +211,6 @@ class NextOrPreviousChannelSelection:
         self.selected_channel = selected_channel
 
 
-class ML:
-    """ML Parent Class to prepare the data and load model
-       Attributes
-       ----------
-            dataset: (Array-like) saves the latest channel state within window size
-            name: (string) name of the model
-            window_size:(int) length of channel state to store in memory (default=10)
-            model_interpreter: tflite interpreter to load model
-
-        Methods
-        -------
-            update_dataset(channel_state)
-
-    """
-
-    def __init__(self, name: str, window_size: int = 10, model_path: str = os.path.abspath("./models/model.tflite")):
-        self.dataset: list = []
-        self.name = name
-        self.window_size: int = window_size
-        self.model_interpreter = tflite.Interpreter(model_path=model_path)
-        # allocate memory
-        self.model_interpreter.allocate_tensors()
-        self.selected_channel = None
-
-    def update_dataset(self, channel_state: list) -> None:
-        """Update the dataset buffer
-
-           Parameters
-           ----------
-                channel_state: (1-D Array-like) state of the channel
-
-            Return 
-            ------
-                None
-        """
-        self.dataset.append(channel_state)
-
-    def get_prediction(self, channel_state) -> None or dict:
-        """Prepare data and invoke model inference
-
-           Parameters
-           ----------
-                channel_state: (1-D Array-like) state of the channel
-
-            Return
-            ------
-                None or predictions
-        """
-        # update dataset and reshape to window size
-        self.update_dataset(channel_state)
-
-        if not len(self.dataset) >= self.window_size:
-            return
-
-        self.dataset = self.dataset[:self.window_size]
-
-        # set input tensor for the model
-        X = np.array([r for r in self.dataset], dtype=np.float32).reshape(
-            (-1, self.window_size, len(channel_state)))
-
-        idx = self.model_interpreter.get_input_details()[0]["index"]
-        # set input tensor
-        self.model_interpreter.set_tensor(idx, X)
-        # invoke predictions
-        self.model_interpreter.invoke()
-
-        idx = self.model_interpreter.get_output_details()[0]["index"]
-        return self.model_interpreter.get_tensor(idx)
-
-    def select_channel(self, channel_state):
-        pass
-
-
 class CS1(ML):
     """Channel Selection Algorithm 1
 
@@ -245,7 +249,7 @@ class CS1(ML):
             self.occ_sums = channel_state
         else:
             self.occ_sums = [r+channel_state[i]
-                         for i, r in enumerate(self.occ_sums)]
+                             for i, r in enumerate(self.occ_sums)]
         self.counter += 1
         return [o / self.counter for o in self.occ_sums]
 
@@ -365,3 +369,67 @@ class CS2(ML):
         self.selected_channel = [
             i for i, val in enumerate(preds) if val == max_preds][0]
         return self.selected_channel
+
+
+class Hoyhtya(CSMethods):
+    """Implement Hoyhtya channel selection algorithm"""
+
+    def __init__(self, name, cursor) -> None:
+        CSMethods.__init__(self, name)
+        self.idle_times = []
+        self.cursor = cursor
+
+    def update_idletimes(self, channel_state):
+        """Update the median idletimes of the channels"""
+        dataset = [[bit for i, bit in enumerate(row) if type(
+            bit) is not str and i != 0] for row in self.cursor.execute(f"""select * from {self.name}""").fetchall()]
+        self.idle_times = [np.median([idletime for s, idletime in idletimes]) if len(
+            idletimes) > 0 else 0 for idletimes in list(map(start_and_idle_time, np.array(dataset).transpose()))]
+
+    def select_channel(self, channel_state):
+        self.update_idletimes(channel_state)
+        if all(channel_state):
+            return
+        max_idletime = np.max(
+            [idletime for i, idletime in self.idle_times if channel_state[i] == 0])
+        if max_idletime == 0:
+            return
+        return [i for i, idletime in enumerate(self.idle_times) if idletime == max_idletime][0]
+
+
+class RenewalTheory(CSMethods):
+    def __init__(self, name, cursor):
+        CSMethods.__init__(self, name)
+        self.idletimes = []
+        self.cursor = cursor
+
+    def update_idletimes(self, channel_state):
+        dataset = [[bit for i, bit in enumerate(row) if type(
+            bit) is not str and i != 0] for row in self.cursor.execute(f"""select * from {self.name}""").fetchall()]
+        mean_ONOFF = [[np.mean(times) if len(times) > 0 else 0 for times in channel] for channel in list(
+            map(on_and_off_time, np.array(dataset).transpose()))]
+        
+        idletimes = []
+        for i, (on, off) in enumerate(mean_ONOFF):
+            lambda_on = np.ma.masked_values(on, 0)**-1
+            lambda_off = np.ma.masked_values(off, 0)**-1
+            if channel_state[i] == 0:
+                prob = lambda_on * (lambda_on + lambda_off)**-1
+                prob += (lambda_off * (lambda_on + lambda_off)**-1) * \
+                    np.exp(-1*(lambda_on + lambda_off))
+            else:
+                prob = lambda_on * (lambda_on + lambda_off)**-1
+                prob -= (lambda_on * (lambda_on + lambda_off)**-1) * \
+                    np.exp(-1*(lambda_on + lambda_off))
+            idletimes.append(prob * np.ma.masked_values(off, 0)**-1)
+
+        self.idletimes = [i if np.ma.is_masked(i) is False else 0 for i in idletimes]
+
+    def select_channel(self, channel_state):
+        if all(channel_state):
+            return
+        max_idletime = np.max(
+            [idletime for i, idletime in self.idletimes if channel_state[i] == 0])
+        if max_idletime == 0:
+            return
+        return [i for i, idletime in enumerate(self.idletimes) if idletime == max_idletime][0]
--- python/specsensor/utils.py
+++ python/specsensor/utils.py
@@ -32,3 +32,41 @@ def start_and_idle_time(bit_sequence):
             idle_times.append((start_time, idle_time))
 
     return np.array(idle_times)
+
+def on_and_off_time(bitseq):
+    bitseq = np.asarray(bitseq, dtype=np.int32)
+    on_time = 0
+    off_time = 0
+    on_times = []
+    off_times = []
+    for i, bit in enumerate(bitseq):
+        # print(i, bit)
+        if i > 0:
+            if bit == 0 and bitseq[i-1] == 1:
+                # 1,0
+                on_times.append(on_time)
+                on_time = 0
+                off_time += 1
+            elif bit == 0 and bitseq[i-1] == 0:
+                # 0,0
+                off_time += 1
+            elif bit == 1 and bitseq[i-1] == 0:
+                # 0,1
+                off_times.append(off_time)
+                off_time = 0
+                on_time += 1
+            else:
+                # 1,1
+                on_time += 1
+        else:
+            if bit == 0:
+                off_time += 1
+            else:
+                on_time += 1
+
+        if i == len(bitseq)-1 and off_time > 0:
+            off_times.append(off_time)
+        if i == len(bitseq)-1 and on_time > 0:
+            on_times.append(on_time)
+
+    return np.array(on_times), np.array(off_times)
